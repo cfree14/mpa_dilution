f1b1729e6021775ae82656ded54a7b0ac46403f6
cfree14 <cfree14@gmail.com>
2018-02-06 12:06:52 -0500

    fixed 2016 to use orig pt/poly files, don't use 2008, formatted TS, fixed problem where pts were ommitted

--- code/Step1_format_wdpa_data.R
+++ code/Step1_format_wdpa_data.R
@@ -17,17 +17,21 @@ library(reshape2)
 library(countrycode)
 
 # Directories
-datadir <- "/data"
+datadir <- "data"
 wdpadir <- "data/wdpa_dbfs"
+wdpadir08 <- "data/wdpa_dbfs_2008"
 wdpadir17 <- "data/WDPA_Dec2017-shapefile"
 
 
-# Learn about data
+# Inspect 2016 data
 ################################################################################
 
+# After inspecting this data, I've decided to use the basic pt and poly files in analysis.
+
 # Data files
-# 1998 point only
-# 2001 and 2008 missing
+# 1998 point data only
+# 2001 missing all data
+# 2008 schema does not match others
 # 2016 has four files (pt + poly buffers)
 dbfs <- list.files(wdpadir)
 
@@ -43,16 +47,80 @@ sum(is.na(pt16$WDPA_PID)) # 0 = no missing
 sum(is.na(pt16buff$WDPA_PID)) # 0 = no missing
 sum(is.na(poly16buff$WDPA_PID)) # 0 = no missing
 sum(is.na(poly16buff$WDPA_PID)) # 0 = no missing
+
+# Are all the pts in pt buffers?
 sum(pt16buff$WDPA_PID%in%pt16$WDPA_PID)==nrow(pt16buff) # all pt buffers in pts
-sum(poly16$WDPA_PID%in%poly16buff$WDPA_PID)==nrow(poly16) # all polys in poly buffers
-sum(pt16$WDPA_PID%in%poly16buff$WDPA_PID)
+sum(pt16$WDPA_PID%in%pt16buff$WDPA_PID)==nrow(pt16) # not all pts in pt buffers (pt16 > pt16buff)
+
+# Are all the polys in poly buffers?
+sum(poly16buff$WDPA_PID%in%poly16$WDPA_PID)==nrow(poly16buff) # not all poly buffers in polys (poly16buff > poly16)
+sum(poly16$WDPA_PID%in%poly16buff$WDPA_PID)==nrow(poly16) # all polys in poly buffers 
+
+# Are points already in polys?
+sum(pt16$WDPA_PID%in%poly16buff$WDPA_PID) # all 15344 buffered points are already in poly16?
+
+# Here's what is happening:
+# poly16buff = poly16 + pt16buff
+# 2016_pnt = 18,975 points
+# 2016_poly = 210,618 polygons
+# 2016_buffpnt = 15,344 of 18,975 with radii (for buffers)
+# 2016_polybuffpnt = 210,618 polygons + 15,344 buffered points = 225,962 total polygons
+
+
+# Inspect 2008 data
+################################################################################
+
+# After inspecting this data, I've decided it is too messy to include in analysis.
+
+# Read 2008 data
+pt08 <- read.dbf(paste(wdpadir08, "v1_01_2008_Pnt.dbf", sep="/"), as.is=T)
+poly08 <- read.dbf(paste(wdpadir08, "v1_01_2008_Poly.dbf", sep="/"), as.is=T)
+colnames(pt08) <- tolower(colnames(pt08))
+colnames(poly08) <- tolower(colnames(poly08))
+
+# Are poly and point WPDAIDs unique? Yes.
+anyDuplicated(pt08$site_id) # must be 0
+anyDuplicated(poly08$site_id)# must be 0
+
+# Are any points in the polygons dataset? No.
+length(pt08$site_id[pt08$site_id%in%poly08$site_id]) # must be 0
+
+# Add shape type and year to point/poly files
+pt08 <- pt08 %>% 
+  mutate(year=2008, shp_type="point")
+poly08 <- poly08 %>% 
+  mutate(year=2008, shp_type="polygon")
+
+# Merge the two datasets
+# and match schema to schema of others
+# The reported area appears 2x: doc_area_h and rep_area 
+# Further investgation revealed that "doc_area_h" has more points than "rep_area" 
+# and contains all of "rep_area"s points
+mpas08 <- rbind.fill(pt08, poly08) %>% 
+  rename(wdpaid=site_id,
+         name=name_eng,
+         orig_name=name_loc,
+         iucn_cat=iucncat,
+         rep_area1=rep_area, # in hectares? same as doc_area_h?
+         rep_area=doc_area_h, # in hectares?
+         gis_area=gis_area_h, # in hectares?
+         rep_m_area=doc_m_area, # in hectares?
+         gov_type=govn_type, 
+         marine=marine_c, 
+         desig_type=sitetype) %>% 
+  select(shp_type, year, wdpaid, name, orig_name, desig_eng, int_crit, status, status_yr, iucn_cat,
+         rep_area, rep_area1, gis_area, rep_m_area, gis_m_area, no_take, no_tk_area, gov_type, 
+         mang_auth, own_type, marine, desig_type, country, iso3) %>% 
+  mutate(rep_area=ifelse(is.na(rep_area), rep_area1, rep_area),
+         iso3=countrycode(iso3, "country.name", "iso3c")) %>% 
+  select(-rep_area1)
 
 
 # Helper functions
 ################################################################################
 
 # Function to format WDPA release
-# polys <- wdpa12poly; pts <- wdpa12pnt; year <- 2012
+# polys <- poly16; pts <- pt16; year <- 2016
 format_wdpa_data <- function(polys, pts, year){
   
   # Lowercase column names
@@ -69,7 +137,9 @@ format_wdpa_data <- function(polys, pts, year){
   
   # Do any WDPAIDs appear in both datasets? If yes, eliminate them.
   pts.with.polys <- pts$wdpaid[pts$wdpaid%in%polys$wdpaid]
-  pts <- subset(pts, wdpaid!=pts.with.polys)
+  if(length(pts.with.polys)>0){
+    pts <- subset(pts, wdpaid!=pts.with.polys)
+  }
   
   # Format datasets before merge
   polys <- polys %>%
@@ -87,7 +157,7 @@ format_wdpa_data <- function(polys, pts, year){
 }
 
 
-# Format data
+# Merge data
 ################################################################################
 
 # Years
@@ -100,19 +170,14 @@ for(i in 1:length(years)){
   # Read data
   yr <- years[i]
   print(yr)
-  if(yr<2016){
+  if(yr==2017){
+    pnts <- read.dbf(paste(wdpadir17, "WDPA_Dec2017-shapefile-points.dbf", sep="/"), as.is=T)
+    polys <- read.dbf(paste(wdpadir17, "WDPA_Dec2017-shapefile-polygons.dbf", sep="/"), as.is=T)
+  }else{
     pt_file <- paste0("temp_wdpa_v2_01_yr_", yr, "_temp_wdpa_v2_01_yr_", yr, "_pnt.dbf")
     poly_file <- paste0("temp_wdpa_v2_01_yr_", yr, "_temp_wdpa_v2_01_yr_", yr, "_poly.dbf")
     pnts <- read.dbf(paste(wdpadir, pt_file, sep="/"), as.is=T)
     polys <- read.dbf(paste(wdpadir, poly_file, sep="/"), as.is=T)
-  }
-  if(yr==2016){
-    pnts <- pt16buff
-    polys <- poly16buff
-  }
-  if(yr==2017){
-    pnts <- read.dbf(paste(wdpadir17, "WDPA_Dec2017-shapefile-points.dbf", sep="/"), as.is=T)
-    polys <- read.dbf(paste(wdpadir17, "WDPA_Dec2017-shapefile-polygons.dbf", sep="/"), as.is=T)
   }
   
   # Format MPAs
@@ -132,8 +197,7 @@ apply(wdpa_ts_orig, 2, function(x) sum(is.na(x)))
 
 # Format WDPA time series
 wdpa_ts <- wdpa_ts_orig %>% 
-  select(-c(verif, metadataid, shape_leng, shape_area, 
-            version, radius_m, buff_dist, orig_fid, wdpaid_d, wdpa_pid_d)) %>% 
+  select(-c(verif, metadataid, shape_leng, shape_area, version, radius_m)) %>% 
   arrange(iso3, wdpaid, wdpa_pid, year)
 
 # Inspect completeness
@@ -188,6 +252,45 @@ wdpa_key[wdpa_key$wdpa_pid==41057,]
 table(wdpa_key$wdpa_yr)
 barplot(table(wdpa_key$wdpa_yr))
   
+
+# Final formats on TS
+################################################################################
+
+# Inspect data
+str(wdpa_ts)
+table(wdpa_ts$shp_type)
+table(wdpa_ts$desig_type)
+table(wdpa_ts$iucn_cat)
+table(wdpa_ts$marine)
+table(wdpa_ts$no_take)
+table(wdpa_ts$status)
+table(wdpa_ts$gov_type) # needs lots of formatting - not worth my time
+
+# Final formatting
+wdpa_ts_final <- wdpa_ts %>%
+  mutate(iucn_cat=revalue(iucn_cat, c("Unknown"="unknown",
+                                      "Not known"="unknown",
+                                      "Not Known"="unknown", 
+                                      "Not Applicable"="not applicable", 
+                                      "Not Assigned"="not assigned",
+                                      "Not Reported"="not reported",
+                                      "Unset"="unset", 
+                                      "\x92W\xc0Ё\xdc\0204"="unknown")),
+         marine=revalue(marine, c("false"="0",
+                                 "N"="0",
+                                 "No"="0",
+                                 "true"="yes", 
+                                 "Y"="yes",
+                                 "Not Reported"="not reported")),
+         no_take=tolower(no_take),
+         status=revalue(status, c("Designated\xa0"="Designated",
+                                  "Desiganted"="Designated", 
+                                  "OB@\xa0\\T\x8e"="Unknown")),
+         status=tolower(status))
+
+# Overwrite
+wdpa_ts_merge <- wdpa_ts
+wdpa_ts <- wdpa_ts_final
 
 # Export data
 ################################################################################
