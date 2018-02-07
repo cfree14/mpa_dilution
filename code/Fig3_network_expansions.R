4ee54c06ef42b779a2794ef296835f8443f3cbd8
cfree14 <cfree14@gmail.com>
2018-02-07 11:11:01 -0500

    updated network expansion code

--- code/Fig3_network_expansions.R
+++ code/Fig3_network_expansions.R
@@ -19,35 +19,57 @@ datadir <- "data"
 plotdir <- "figures"
 
 # Load data
-load(paste(datadir, "2014-17_network_change_events_marine.Rdata", sep="/"))
+load(paste(datadir, "2014-17_network_change_events.Rdata", sep="/"))
+
+
+# Build data
+################################################################################
+
+# Inspect "marine" types
+table(mpas_changed_all$marine)
+
+# Number of changes per year
+changes_yr <- mpas_changed_all %>%
+  mutate(marine=ifelse(is.na(marine), "unknown", marine), 
+         marine=revalue(marine, c("0"="terrestrial",
+                                  "1"="terrestrial/marine", 
+                                  "2"="marine", 
+                                  "not reported"="unknown",
+                                  "yes"="both/marine"))) %>% 
+  group_by(year, marine, change_type) %>% 
+  summarize(n=n()) %>% 
+  spread(change_type, n)
 
 
 # Plot data
 ################################################################################
 
-# Number of changes per year
-changes_yr <- mpas_changed_all %>%
-  group_by(year, change_type) %>% 
-  summarize(n=n()) %>% 
-  spread(change_type, n)
-
-# Sample size
-n_neg <- sum(changes_yr$lost)
-n_pos <- sum(changes_yr$added)
-
 # Setup figure
 figname <- "Fig3_network_changes_by_year.png"
-png(paste(plotdir, figname, sep="/"), width=5, height=4, units="in", res=600)
-par(mar=c(5,4,0.5, 0.5), mgp=c(3,0.8,0))
+png(paste(plotdir, figname, sep="/"), width=6, height=4, units="in", res=600)
+par(mfrow=c(2,3), mar=c(5,4,1.5, 0.5), mgp=c(3,0.8,0))
 
-# Plot 
-barplot(t(as.matrix(changes_yr[,3:2])), beside=T, las=2, col=c("red", "blue"), 
-        names=changes_yr$year, ylab="# of events")
+# Loop through types
+types <- c("marine", "terrestrial", "terrestrial/marine", "both/marine", "unknown")
+for(i in 1:length(types)){
+  
+  # Subset data
+  sdata <- subset(changes_yr, marine==types[i])
+  sdata1 <- t(as.matrix(sdata[,c("lost", "added")]))
+  
+  # Plot 
+  barplot(sdata1, beside=T, las=2, col=c("red", "blue"), 
+          cex.names=0.8, cex.axis=0.8, cex.lab=0.8,
+          names=sdata$year, ylab="# of network expansions", main=types[i])
+  
+  # Add legend
+  # legend_text <- c(paste0("Reductions (n=", n_neg, ")"),
+  #                  paste0("Expansions (n=", n_pos, ")"))
+  # legend("topleft", legend=legend_text , fill=c("red", "blue"), bty="n", cex=0.8)
+  
+}
 
-# Add legend
-legend_text <- c(paste0("Reductions (n=", n_neg, ")"),
-                 paste0("Expansions (n=", n_pos, ")"))
-legend("topleft", legend=legend_text , fill=c("red", "blue"), bty="n", cex=0.8)
+
 
 # Off
 dev.off()
