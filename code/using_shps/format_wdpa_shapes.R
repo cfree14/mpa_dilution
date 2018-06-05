ce7065c5505ecb237e5bd61b3031de1c185f20a2
cfree14 <cfree14@gmail.com>
2018-06-05 11:52:51 -0400

    moved format WDPA shapes and added area calculation from 2017 WDPA (not in temporal WDAP)

--- code/using_shps/format_wdpa_shapes.R
+++ code/using_shps/format_wdpa_shapes.R
@@ -21,12 +21,36 @@ library(countrycode)
 # Directories
 datadir <- "data"
 wdpadir <- "data/wdpa_shps"
+wdpa17dir <- "data/WDPA_Dec2017-shapefile"
 outputdir <- "data/wdpa_shps/temporal_wdpa_tables_with_corrected_areas"
 
 # Read geodatabase and inspect feature classes
 fgdb <- paste(wdpadir, "temp_WDPA_output_v2_02.gdb", sep="/")
 fc_list <- ogrListLayers(fgdb)
 print(fc_list)
+
+# Do 2017 polygon first
+################################################################################
+
+# Read WDPA 2017 polygon file
+poly17 <- readOGR(dsn=wdpa17dir, layer="WDPA_Dec2017-shapefile-polygons")
+
+# Project to Mollweide
+moll <- CRS("+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+fc_moll <- spTransform(fc, moll)
+
+# Calculate area (sq. km)
+# Mollweide is in m so areas are in m2
+# km2 = m2 / 1000000
+area_sqkm <- rgeos::gArea(fc_moll, byid=T) / 1000000
+
+# Add area to table
+data <- fc_moll@data
+data$gis_area_sqkm <- area_sqkm
+
+# Export table
+outfile <- paste0(poly, ".Rds")
+saveRDS(data, file=paste(outputdir, outfile, sep="/"))
 
 
 # Build data
@@ -37,7 +61,7 @@ polys <- sort(fc_list[grepl("*poly$", fc_list)])
 
 # Loop through polygon features
 for(i in 11:length(polys)){
-
+  
   # Read feature class
   poly <- polys[i]
   print(paste(i, poly))
